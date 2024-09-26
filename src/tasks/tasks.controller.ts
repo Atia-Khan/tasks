30628c4ad7dd1f3dd01d6fe833271a0ae47a7108
Atia-Khan <atia.khan@xloopdigital.com>
2024-09-26 13:43:50 +0500

    endpoint tasks getByProjectId created

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,36 +1,40 @@
 import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TaskDto } from '../tasks/dto/tasks.dto'; 
+import { Tasks } from './entities/task.entity';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  // Create a new task
+  
   @Post()
   async create(@Body() taskDto: TaskDto) {
     return this.tasksService.create(taskDto);
   }
 
-  // Get all tasks
+  
   @Get()
   async findAll() {
     return this.tasksService.findAll();
   }
-
-  // Get a task by ID
+ 
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.tasksService.findOne(id);
   }
+  
+  @Get('project/:projectId')
+  async findByProjectId(@Param('projectId') projectId: string): Promise<Tasks[]> {
+      return this.tasksService.findByProjectId(projectId);
+  }
 
-  // Update a task
+
   @Put(':id')
   async update(@Param('id') id: string, @Body() taskDto: Partial<TaskDto>) {
     return this.tasksService.update(id, taskDto);
   }
 
-  // Delete a task
   @Delete(':id')
   async delete(@Param('id') id: string) {
     return this.tasksService.delete(id);
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -8,18 +8,22 @@ import { TaskDto } from '../tasks/dto/tasks.dto';
 export class TasksService {
   constructor(@InjectModel(Tasks.name) private readonly tasksModel: Model<TasksDocument>) {}
 
-  // Create a new task
+  
   async create(taskDto: TaskDto): Promise<Tasks> {
     const newTask = new this.tasksModel(taskDto);
     return newTask.save();
   }
 
-  // Retrieve all tasks
+
   async findAll(): Promise<Tasks[]> {
     return this.tasksModel.find().exec();
   }
 
-  // Find a task by ID
+
+  async findByProjectId(projectId: string): Promise<Tasks[]> {
+    return this.tasksModel.find({project_id: projectId}).exec()
+  }
+
   async findOne(id: string): Promise<Tasks> {
     const task = await this.tasksModel.findById(id).exec();
     if (!task) {
@@ -28,7 +32,7 @@ export class TasksService {
     return task;
   }
 
-  // Update a task
+ 
   async update(id: string, taskDto: Partial<TaskDto>): Promise<Tasks> {
     const updatedTask = await this.tasksModel.findByIdAndUpdate(id, taskDto, { new: true }).exec();
     if (!updatedTask) {
@@ -37,11 +41,13 @@ export class TasksService {
     return updatedTask;
   }
 
-  // Delete a task
+  
   async delete(id: string): Promise<void> {
     const result = await this.tasksModel.findByIdAndDelete(id).exec();
     if (!result) {
       throw new NotFoundException(`Task with ID ${id} not found`);
     }
   }
+
+
 }
