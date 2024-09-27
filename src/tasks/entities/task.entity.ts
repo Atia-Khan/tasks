07c452ea71c369641ec8ed2d25df1f99bdb75acf
Atia-Khan <atia.khan@xloopdigital.com>
2024-09-27 13:24:56 +0500

    app_rej_by_id and name refectored

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,50 +1,45 @@
-
-
-
-import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-
 
 export type TasksDocument = Tasks & Document;
 
-@Schema({timestamps: true, collection: 'tl_tasks'})
+@Schema({ timestamps: true, collection: 'tl_tasks' })
 export class Tasks {
+  @Prop({ required: true })
+  name: string;
 
-@Prop({required: true})
-name: string;
+  @Prop({ required: true })
+  project_id: string;
 
-@Prop({required: true})
-project_id : string;
+  @Prop({ required: true })
+  project_name: string;
 
-@Prop({required:  true})
- project_name : string;
+  @Prop({ required: true })
+  created_by_id: string;
 
-@Prop({required: true})
- created_by_id : string;
+  @Prop({ required: true })
+  created_by_name: string;
 
-@Prop({required: true})
- created_by_name: string;
+  @Prop({ required: true })
+  estimated_hours: number;
 
-@Prop({required: true})
- estimated_hours: number;
+  @Prop({ required: true })
+  estimated_minutes: number;
 
-@Prop({required: true})
- estimated_minutes: number;
+  @Prop({ required: true })
+  estimated_days: number;
 
-@Prop({required: true})
- estimated_days: number;
+  @Prop({ default: null })
+  app_rej_by_id: string | null;
 
-@Prop({required: true})
- app_rej_by_id: string;
+  @Prop({ default: null })
+  app_rej_by_name: string | null;
 
-@Prop({required: true})
- app_rej_by_name: string;
+  @Prop({ required: true })
+  status: string;
 
-@Prop({required: true})
- status: string;
-
-@Prop({required: true})
-_active: boolean;
+  @Prop({ required: true })
+  _active: boolean;
 }
 
-export const TasksSchema = SchemaFactory.createForClass(Tasks)
+export const TasksSchema = SchemaFactory.createForClass(Tasks);
